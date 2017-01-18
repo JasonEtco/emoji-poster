cd1981cbe4fe5fc81adc717e2bc6dd84c6a14c27
JasonEtco <jasonetco@gmail.com>
2017-01-17 21:12:10 -0500

    :lock: Hide broken male/female emojis

--- emojis.js
+++ emojis.js
@@ -3,7 +3,11 @@ const emojis = require('emojilib');
 const wrapper = document.querySelector('.wrapper');
 
 emojis.ordered.forEach(e => {
+  const { char } = emojis.lib[e];
+
+  if (char && (char.endsWith('♀️') || char.endsWith('♂️'))) return;
+
   const emojiEl = document.createElement('div');
-  emojiEl.textContent = emojis.lib[e].char;
+  emojiEl.textContent = char;
   wrapper.appendChild(emojiEl);
 });
