de0dbb1848ab3ca1e20201e055d688cb1c9a2361
JasonEtco <jasonetco@gmail.com>
2017-01-17 21:48:40 -0500

    :eggplant: Remove broken emojis and flags

--- emojis.js
+++ emojis.js
@@ -2,9 +2,40 @@ const emojis = require('emojilib');
 
 const wrapper = document.querySelector('.wrapper');
 
-emojis.ordered.forEach(e => {
-  const { char } = emojis.lib[e];
+const brokenEmojis = [
+  'copyright',
+  'registered',
+  'tm',
+  'rainbow_flag',
+  'arrow_upper_right',
+  'arrow_lower_left',
+  'left_right_arrow',
+  'spades',
+  'clubs',
+  'hearts',
+  'diamonds',
+  'black_small_square',
+  'white_small_square',
+  'zero',
+  'hash',
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'keycap_star',
+  'bangbang',
+];
 
+emojis.ordered.forEach(e => {
+  const { char, category } = emojis.lib[e];
+
+  if (brokenEmojis.indexOf(e) !== -1) return;
+  if (category === 'flags') return;
   if (char && (char.endsWith('♀️') || char.endsWith('♂️'))) return;
 
   const emojiEl = document.createElement('div');
